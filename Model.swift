dc6c8886e064cde96a76a8c9c851760dee13767b
tomblasta <tomwes.black@bigpond.com>
2014-09-19 16:57:54 +1000

    Initial Commit
    test

---
+++ Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  TapMe
+//
+//  Created by Thomas Black on 19/09/2014.
+//  Copyright (c) 2014 Thomas Black. All rights reserved.
+//
+
+import Foundation
+
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapMe
+//
+//  Created by Thomas Black on 19/09/2014.
+//  Copyright (c) 2014 Thomas Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
